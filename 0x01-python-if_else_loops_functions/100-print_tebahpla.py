b19f25592ae68b358287667f60b5bba60cca73d5
Kmag20 <owenmawira@gmail.com>
2023-11-29 14:27:51 -0800

    rev alpha

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for letter in range(122, 96, -1):
     if letter % 2 == 0:
-        print(f"{chr(letter)}", end="")
+        print("{}".format(chr(letter)), end="")
 
     else:
-        print(f"{chr(letter - 32)}", end="")
+        print("{}".format(chr(letter - 32)), end="")
