d1776ca868b3d1d821f85305f2061f92d58fdbae
Kmag20 <owenmawira@gmail.com>
2024-01-15 10:18:52 -0800

    Base Class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """ Module that contains class Base """
 class Base:
-    """ Class Base """
+     """Represent the base model.
+
+    Represents the "base" for all other classes in project 0x0C*.
+
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
         if id is not None:
             self.id = id
         else:
