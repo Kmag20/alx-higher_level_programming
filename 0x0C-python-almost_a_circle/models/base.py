cfdd9a8c8cd6f9ca60ce6de7e06bd05f13471370
Kmag20 <owenmawira@gmail.com>
2024-01-15 10:13:30 -0800

    Base Class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-
+""" Module that contains class Base """
 class Base:
+    """ Class Base """
     __nb_objects = 0
 
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects = 0
+            Base.__nb_objects += 1
             self.id = __nb_objects
 
