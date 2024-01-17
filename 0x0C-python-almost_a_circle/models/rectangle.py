27f3263ef530adfb67aeb551178780e0d6a36310
Kmag20 <owenmawira@gmail.com>
2024-01-17 01:48:26 -0800

    draw rect

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,47 +1,14 @@
 #!/usr/bin/python3
+""" Rectangle class inherits from Base Class """
 
-from base import Base
+from models.base import Base
+
 
 class Rectangle(Base):
-    def __init__(self, width, height, x, y, id):
+    """ Rectangle Base """
+    def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
         super().__init__(id)
-
-        @property
-        def width(self):
-            return self.__width
-
-        @width.setter
-        def width(self, value):
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value <= 0:
-                raise ValueError("width must be > 0")
-
-            self.__width = width
-
-        @property
-        def height(self):
-            return self.__height
-
-        @height.setter
-        def height(self, value):
-            if not isinstance(value, int):
-                raise TypeError("Height must be an integer")
-            if value < 0:
-                raise ValueError("height must be > 0")
-
-            self.__height = height
-           
-        @property
-        def x(self):
-            return self.__x
-
-        @x.setter
-        def x(self, value):
-        
-
-
