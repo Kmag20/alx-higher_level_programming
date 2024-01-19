c0bc7e2b1abf97a8d297de5c2eeb9a276cd835f9
Kmag20 <owenmawira@gmail.com>
2024-01-19 03:47:10 -0800

    dict to json repr

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_dictionary(list_dictionaries):
+        """ returns json representation of list dicts """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
