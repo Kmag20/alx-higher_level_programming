a853e4a654a2eccd4724e2d9c8b29e492bb9b136
Kmag20 <owenmawira@gmail.com>
2024-01-19 02:01:02 -0800

    Square Class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,3 +7,13 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ returns an informal representation of the Square instance """
+
+        return "[{}] ({}) {}/{} - {}".format(
+                self.__class__.__name__,
+                self.id, 
+                self.x,
+                self.y
+                self.width)
