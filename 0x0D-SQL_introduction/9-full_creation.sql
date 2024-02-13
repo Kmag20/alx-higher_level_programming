e8a4b736d5cab084616e3f681243bd09851d10ee
Kmag20 <owenmawira@gmail.com>
2024-02-13 10:46:42 -0800

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Creates a second table of the database hbtn_0c_0
-CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256) `score` INT);
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
