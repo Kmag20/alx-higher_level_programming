406c047a407939e25a94d79f79a6a899a7cbf8d4
Kmag20 <owenmawira@gmail.com>
2024-03-09 12:46:36 +0300

    DS

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,21 @@
 def roman_to_int(roman_string):
     """ Converts a Roman Numeral to an integer """
     thousands = {'MMM': 3000, 'MM': 2000, 'M': 1000}
-    hundreds = {'CM': 900, 'DCCC': 800, 'DCC': 700, 'DC': 600, 'D': 500, 'CD': 400, 'CCC': 300, 'CC': 200, 'C': 100}
-    tens = {'XC': 90, 'LXXX': 80 , 'LXX': 70 , 'LX': 60, 'L': 50, 'XL': 40, 'XXX': 30, 'XX': 20, 'X': 10}
-    units = {'IX': 9, 'VIII': 8, 'VII': 7, 'VI': 6, 'V': 5, 'IV': 4, 'III': 3, 'II': 2, 'I': 1}
-
+    hundreds = {'CM': 900, 'DCCC': 800, 'DCC': 700,
+                'DC': 600, 'D': 500, 'CD': 400, 'CCC': 300,
+                'CC': 200, 'C': 100}
+    tens = {'XC': 90, 'LXXX': 80, 'LXX': 70,
+            'LX': 60, 'L': 50, 'XL': 40, 'XXX': 30,
+            'XX': 20, 'X': 10}
+    units = {'IX': 9, 'VIII': 8, 'VII': 7, 'VI': 6, 'V': 5, 'IV': 4,
+             'III': 3, 'II': 2, 'I': 1}
     number = 0
+    if not isinstance(roman_string, str):
+        return number
+
+    if roman_string is None:
+        return number
+
     for roman, value in thousands.items():
         if roman in roman_string:
             number += value
@@ -28,14 +38,14 @@ def roman_to_int(roman_string):
                 number += 0
             if 'XL' in roman_string:
                 number += tens['XL']
-            else: 
+            else:
                 number += value
             break
     for roman, value in units.items():
         if roman in roman_string:
             if 'IV' in roman_string:
                 number += units['IV']
-            else:    
+            else:
                 number += value
             break
 
