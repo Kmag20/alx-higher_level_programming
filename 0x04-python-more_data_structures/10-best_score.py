7fe3e2545c91bb7021acc2e368093d664945bceb
Kmag20 <owenmawira@gmail.com>
2024-03-09 16:22:29 +0300

    DS

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,8 @@
 
 def best_score(a_dictionary):
     """ Returns a key with the biggest integer value """
-    if len(a_dictionary) == 0 or not isinstance(a_dictionary, dict):
+    if len(a_dictionary) == 0 or not isinstance(a_dictionary, dict)
+    or a_dictionary is None:
         return None
     max_value = max(a_dictionary.values())
     for key, val in a_dictionary.items():
