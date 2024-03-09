d4e2f1af8d3c2022aee3303eb491dcee307603bc
Kmag20 <owenmawira@gmail.com>
2024-03-09 12:56:08 +0300

    DS

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 
 def best_score(a_dictionary):
     """ Returns a key with the biggest integer value """
+    if len(a_dictionary) == 0 or not isinstance(a_dictionary, dict):
+        return None
     max_value = max(a_dictionary.values())
     for key, val in a_dictionary.items():
         if val == max_value:
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,5 @@
 
 def multiply_by_2(a_dictionary):
     """ Returns a new dictionary with all vals multiplied by 2 """
-    for key, value in a_dictionary.items():
-        a_dictionary[key] = value * 2
-    return a_dictionary
+    return {a_dictionary[key] = value * 2
+            for key, value in a_dictionary.items()}:
