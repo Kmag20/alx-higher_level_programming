66afcbdab81b4fc6779eba73967021bca99b97fd
Kmag20 <owenmawira@gmail.com>
2024-03-13 19:04:29 +0300

    Classes and Objects

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+class Square:
+    """ class Square """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ Getter method for size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Setter method for size """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
