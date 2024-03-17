abb37f009876186e2b653c46bd1a657f3d2723e9
Kmag20 <owenmawira@gmail.com>
2024-03-17 11:48:52 +0300

    Filter using wildcards/symbols

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Lists all states with a name starting with N """
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3],
+                           port=3306)
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states \
+                   WHERE name LIKE 'N%'")
+    query = cursor.fetchall()
+    [print(row) for row in query]
