a3b4d77096cec30d82375105902a404ead6b24fe
Kmag20 <owenmawira@gmail.com>
2024-03-18 22:17:05 +0300

    Fetch One row

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Script that prints the first State object
+    from the database hbtn_0e)6_usa """
+
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_base import State
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).first()
+    print("{}: {}".format(state.id, state.name))
