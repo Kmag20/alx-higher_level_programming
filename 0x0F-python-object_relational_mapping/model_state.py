ec7b25bf80964cb3cdd469cc1ff3677087f40a35
Kmag20 <owenmawira@gmail.com>
2024-03-18 18:02:29 +0300

    BaseModel

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,7 +12,6 @@ class State(Base):
     """ Defines State cls """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True,
-         autoincrement=True, nullable=False
-         unique=True)
+                autoincrement=True, nullable=False
+                unique=True)
     name = Column(String(128), nullable=False)
-
