3a0c8eac55ba1b0e04730b9d2b7858fce43dace1
Kmag20 <owenmawira@gmail.com>
2024-03-18 17:43:10 +0300

    CaseSensitive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,10 @@ if __name__ == '__main__':
                            port=3306)
 
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE \
-                   name COLLATE Latin1_General_CS LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
     query = cursor.fetchall()
     [print(row) for row in query]
