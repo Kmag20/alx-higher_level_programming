fae3445d6e14ef188e84b5142be67850f768664a
Kmag20 <owenmawira@gmail.com>
2024-03-19 08:01:00 +0300

    two DB's

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,10 +5,10 @@ from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
+
 class City(Base):
     """ Defines the City cls """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer,ForeignKey('states.id'), nullable=False)
-
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
