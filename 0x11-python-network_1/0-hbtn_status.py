101b003043024eb66b223f56f118fdb264f355ae
Kmag20 <owenmawira@gmail.com>
2024-03-29 17:45:26 +0300

    Status Code

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Python script that fetches https://alx-intranet.hbtn.io/status
+
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    status = response.read()
+
+print(f"Body response\n\t- type: {type(status)}\n\t- content: {status}\n\t- utf8 content: {status.decode('utf-8')}\n")
