bb536e2b4404e5b454ff64cf70fc29d50d98a85d
Kmag20 <owenmawira@gmail.com>
2024-03-29 22:22:47 +0300

    Error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,8 +2,10 @@
 """
 Python script that takes in a URL, sends a request and displays
 the value of the X-Request-Id variable found in the header
-(handling HTTP errors)import urllib.error
+(handling HTTP errors)
 """
+
+import urllib.error
 import urllib.request
 import sys
 
