8b38a30e5134da3d28fd7dcf030a69a90d013366
Kmag20 <owenmawira@gmail.com>
2024-03-29 22:21:01 +0300

    Error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-Sends a req to a URL and handles the response and also
-the error
+Python script that takes in a URL, sends a request and displays
+the value of the X-Request-Id variable found in the header
+(handling HTTP errors)import urllib.error
 """
-import urllib.error
 import urllib.request
 import sys
 
