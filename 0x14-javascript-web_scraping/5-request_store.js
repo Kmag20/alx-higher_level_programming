bc7bea764307778abfcefe561dd0d641dac2194e
Kmag20 <owenmawira@gmail.com>
2024-04-24 18:15:17 +0300

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
 const fs = require('fs');
-url = process.argv[2];
-outputFile = process.argv[3];
-request.get(url,  (err, res, body) => {
+const url = process.argv[2];
+const outputFile = process.argv[3];
+request.get(url, (err, res, body) => {
   if (!err && res.statusCode === 200) {
     fs.writeFile(outputFile, body, (err) => {
       if (err) console.error(err);
