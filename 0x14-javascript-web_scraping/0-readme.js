f2e283037f233c6d1bb8f8adc70fe5d4edbf379f
Kmag20 <owenmawira@gmail.com>
2024-04-24 10:07:26 +0300

    read file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const filename = process.argv[2];
-
-fs.readFile(filename, 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
-  } else {
-    console.log(data);
-  }
+  console.log(data);
 });
