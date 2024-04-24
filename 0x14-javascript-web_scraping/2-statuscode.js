d69856ee7b3b0f8f7b5b8d961ce74e83ca8c86b3
Kmag20 <owenmawira@gmail.com>
2024-04-24 11:38:22 +0300

    request module

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+request.get(process.argv[2], function(err, res, data) {
+  if (err) console.error(err);
+  console.log(`code: ${res.statusCode}`);
+});
