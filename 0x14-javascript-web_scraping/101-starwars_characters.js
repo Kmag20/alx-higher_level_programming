07b0c16640b67eef3a2470ea0a6a28023ac49d63
Kmag20 <owenmawira@gmail.com>
2024-04-26 20:12:58 +0300

    Star Wars

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const index = parseInt(process.argv[2], 10);
+request.get(url, (err, res, body) => {
+  if (!err && res.statusCode === 200) {
+    const data = JSON.parse(body);
+    const characters = data.results[index - 1].characters;
+    characters.forEach(character => {
+      request.get(character, (err, res, body) => {
+        if (!err && res.statusCode === 200) {
+          const charName = JSON.parse(body).name;
+          console.log(charName);
+        }
+      });
+    });
   }
 });
-
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-  });
-}
